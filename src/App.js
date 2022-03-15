923d39066c4fdd5b19738e7cbfba8a76b82e2d6c
Max Shilov <dr.thomas.hewitt@gmail.com>
2022-03-15 23:37:15 +0300

    Setup for GH Pages

--- src/App.js
+++ src/App.js
@@ -13,11 +13,14 @@ function App() {
         <Navbar />
         <div className="container pt-4">
           <Routes>
-            <Route path="/" element={<Characters />} />
-            <Route path="/allegiances" element={<Allegiances />}>
+            <Route path="/tango-test-app/" element={<Characters />} />
+            <Route path="/tango-test-app/allegiances" element={<Allegiances />}>
               <Route path=":houseId" element={<Allegiances />} />
             </Route>
-            <Route path="*" element={<h1>Oops... there is nothing here!</h1>} />
+            <Route
+              path="tango-test-app/*"
+              element={<h1>Oops... there is nothing here!</h1>}
+            />
           </Routes>
         </div>
       </BrowserRouter>
